442bf4d745086a5c68f1ac6e46f99a3f0743006b
Jaroslav Vysoky <vysoky@borka.cz>
2012-03-02 17:30:33 +0100

    reverse.py

---
+++ reverse.py
@@ -0,0 +1,12 @@
+import sys
+
+vstup = "xxxx"
+while vstup and vstup.count("konec") == 0:
+    vstup = sys.stdin.readline()
+    ret = vstup.replace("\n","")
+    iret = ""
+    for i in ret:
+        iret = i + iret
+    sys.stdout.write(iret + "\n")
+    
+    
